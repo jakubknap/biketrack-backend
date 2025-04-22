48c6b5743db4ac22c799f03719f55f4649985c87
Jakub Knap <jknap40@gmail.com>
2025-04-22 20:52:02 +0200

    initial commit

---
+++ src/test/java/pl/biketrack/BikeTrackApplicationTests.java
@@ -0,0 +1,12 @@
+package pl.biketrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
